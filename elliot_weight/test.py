75e659f201c5704aca4d237d93cca9e36fd9beaf
Ian Buckley <ian.m.buckley@gmail.com>
2019-05-05 14:30:05 +0200

    added some things

--- elliot_weight/test.py
+++ elliot_weight/test.py
@@ -4,16 +4,23 @@
 
 import datetime as dt
 
-dates = ['01/13/2019','01/16/2019','02/21/2019', '04/02/2019', '04/15/2019', '04/18/2019','04/19/2019','04/20/2019','04/21/2019','04/22/2019','04/23/2019', '04/24/2019', '04/25/2019', '04/28/2019', '04/29/2019']
+dates = ['01/13/2019','01/16/2019','02/21/2019', '04/02/2019', '04/15/2019',
+         '04/18/2019','04/19/2019','04/20/2019','04/21/2019','04/22/2019',
+         '04/23/2019', '04/24/2019', '04/25/2019', '04/28/2019', '04/29/2019',
+         '05/01/2019', '05/02/2019', '05/05/2019']
 x = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in dates]
-y = [3250,2940,4140,4650,4990,5090,5210,5190,5430,5190,5310,5290,5360,5400,5470]
+y = [3250,2940,4140,4650,4990,5090,5210,5190,5430,5190,5310,5290,5360,5400,5470,5580,5590,5640]
 #y = range(len(x)) # many thanks to Kyss Tao for setting me straight here
+
+
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-plt.plot(x,y)
+x1 = x[4:]
+y1 = y[4:]
+plt.plot(x1,y1)
 plt.gcf().autofmt_xdate()
 plt.show()
